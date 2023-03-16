084a8c55f4f3afe95977cbd567da46f7c87596ee
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-16 13:51:56 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
+/* A program that prints by using the 'puts' command */
 #include <stdio.h>
 
-int main(void){
-  puts("Programming is like building a multilingual puzzle,");
-  return (0);
+int main(void)
+{
+ puts("Programming is like building a multilingual puzzle");
+ return (0);
 }

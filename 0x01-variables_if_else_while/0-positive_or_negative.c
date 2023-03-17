76c157b1382b6ce08d00484eabdce2ed158dcd15
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-17 13:48:58 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is negative\n, n");
+printf("n is negative\n");
 }
 else if (n == 0)
 {
-printf("is zero\n, n");
+printf("n is zero\n");
 }
 else  /*(n < 0)*/
 {
-printf("is positive\n, n");
+printf("n is positive\n");
 };
 return (0);
 }

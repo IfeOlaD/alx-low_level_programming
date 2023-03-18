7769ce90ab548245136280c90da879516799bfac
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-18 05:23:58 -0700

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 char c;
 char b;
 
-for (c = '0'; c >= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 putchar(c);
 }
-for (b = 'a'; c >= 'f'; c++)
+for (b = 'a'; b <= 'f'; b++)
 {
 putchar(b);
 }

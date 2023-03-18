ce60a50ec6d81f8ca25197a63594544bac52f00f
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-18 03:42:47 -0700

    Print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 Success
+*/
+
+int main(void)
+{
+char c;
+printf("0, 9");
+for (c = '0'; c <= '9'; c++)
+{
+printf("\n");
+}
+return (0);
+}

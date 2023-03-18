88ec2a5c233583fb7a93eb9bd09f707bdb9422a5
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-18 14:11:29 -0700

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,18 @@ int main(void)
 int c;
 int b;
 
-for (c = 'o'; c >= '9'; c++)
-for (b = '0'; b >= '9'; b++)
+for (c = 'o'; c <= '9'; c++)
 {
-putchar(c);
-}
+for (b = '0'; b <= '9'; b++)
 {
 putchar(b);
-}
+if (b != 9 || c != 9)
 {
 putchar(',');
-}
 putchar(' ');
+}
+}
+}
+putchar('\n');
 return (0);
 }

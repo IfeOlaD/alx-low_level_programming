1e2a25145f422bf9f0830a0029a78c92b74389a6
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-21 21:18:19 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* print_alphabet - Entry point
+*
+* Return: Always 0 Success
+*/
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+}

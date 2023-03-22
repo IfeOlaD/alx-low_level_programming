d3cc9dc4aa0c035d26cf1530a1b745857ee71ba0
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-22 14:14:27 -0700

    Isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isalpha - Checks if character is upper case or lower or lower case
+*
+* @c: The character to check
+*
+* Return: 0 or 1
+*/
+
+int _isalpha(int c)
+{
+if (c >= 'a' || c >= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

24aabe047cd8eb3b4bd740889c6a4dd0166a66cd
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-22 08:21:56 -0700

    Alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 int b = 0;
 char c;
 
-while (b <= 9)
+while (b < 10)
 {
 c = 'a';
 while (c <= 'z')
@@ -20,5 +20,6 @@ _putchar(c);
 c++;
 }
 _putchar('\n');
+b++;
 }
 }

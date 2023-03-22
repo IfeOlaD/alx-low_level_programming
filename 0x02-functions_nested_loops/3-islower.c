2933c0c2459524d2d8d7089333f149b4390fe69a
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-22 13:56:07 -0700

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _islower - Checks for lower case
+*
+* c is the declared funtion
+*
+* Return: 0 or 1
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

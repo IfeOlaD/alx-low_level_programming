a784a9c38e81eea29ffe352d0838da13324a69cb
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-24 05:24:05 -0700

    Print comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 int b;
 int c;
 int a;
-for (b = '0'; b <= '9'; b++)
+for (b = 0; b <= 10; b++)
 {
-for (c = '0'; c <= '9'; c++)
+for (c = 0; c <= 10; c++)
 {
-for (a = '0'; a <= '9'; a++)
+for (a = 0; a <= 10; a++)
 {
 putchar(b + '0');
 putchar(c + '0');

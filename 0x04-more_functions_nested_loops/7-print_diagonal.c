c66d0ce4f6365f39074f3b7864598d5ffa2ec419
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-24 13:17:34 -0700

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a set of character diagonally
+ *
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+int a;
+int b;
+if(n > 0)
+{
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}

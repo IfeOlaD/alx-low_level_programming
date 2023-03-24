717a7cae9d9b67b333d4d58dfb1001fd029b7761
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-24 09:06:05 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints all minutes of 24 hours
+ * 
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+int c;
+for (c = 00:00; c <= 23:59; c++)
+_putchar(c);
+}

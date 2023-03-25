82009b75aa9a1a18748a27263845d9d98ea39af3
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-25 14:56:37 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: the integer
+ * Return: o
+ */
+
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+{
+_putchar(',');
+_putchar(' ');
+}
+}

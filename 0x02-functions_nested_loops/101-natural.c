aab8de75318999d6a37ba70b5eb371b771dce6f0
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-26 07:54:11 -0700

    Natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,13 @@ int main(void)
 {
 int a;
 int b = 0;
-for (a = 0; a < 1024; a += 1)
+for (a = 0; a < 1024; a++)
 {
 if (a % 3 == 0 && a % 5 == 0)
-b = b + a;
+{
+b += 1
 }
-printf("\n");
+}
+printf("%d\n", b);
 return (b + a);
 }

a9e26743c762042678e91fb2833ae9be77119e61
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-27 15:17:15 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: the integer
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return len;
+}

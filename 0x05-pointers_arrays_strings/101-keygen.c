e79fe318557642d91e74f04b9c274200a1d2d2a8
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-28 17:15:12 -0700

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,36 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- *
+ * main - generates password
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+char password[100];
+int sum = 0;
+int target_sum = 2772;
+
+srand(time(NULL));
+
+for (a = 0; a < 100; a++)
+{
+password[a] = rand() % 94 + 33;
+sum += password[a];
+if (sum >= target_sum)
+break;
+}
+if (sum != target_sum)
+{
+password[a - 1] -= (sum - target_sum);
+a++;
+}
+password[a] = '\0';
+
+printf("%s\n", password);
+
+return (0);
+}

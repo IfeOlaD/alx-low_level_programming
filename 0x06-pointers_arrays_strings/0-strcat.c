61b026308a63ac0cc6c2f3b035535b3907a06eed
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-30 14:20:18 -0700

    Contatenate String

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * *_strcat - Contatenates 2 strings
@@ -14,28 +13,10 @@ char *_strcat(char *dest, char *src)
 int a = 0;
 int b = 0;
 
-while (dest[a] != '\0')
-{
-a++;
-}
-while (src[b] != '\0')
-{
-dest[a] = src[b];
-a++;
-b++;
-}
-dest[a] = '\0';
+for (a = 0; dest[a] != '\0'; a++)
+;
+for (b = 0; src[b] != '\0'; b++)
+dest[a + b] = src[b];
+dest[a + b] = '\0';
 return (dest);
-}
-
-int main(void)
-{
-char a[100] = "Hello";
-char b[100] = "World";
-
-strcat(a, b);
-
-printf("%s\n", a);
-
-return (0);
 }

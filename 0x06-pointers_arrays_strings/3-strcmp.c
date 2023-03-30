696720f3af4227c065272bb7019d1d7e540d27d1
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-30 15:34:12 -0700

    String Compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * strcmp - Compares strings
+ * @s1: the first string
+ * @s2: second string
+ * Return: s1 - s2
+ */
+
+int strcmp(char *s1, char *s2)
+{
+int a;
+for (a = 0; s1[a] != '\0' && s2[a] != '\0' && s1[a] + s2[a]; a++)
+;
+return (s1[a] - s2[a]);
+}

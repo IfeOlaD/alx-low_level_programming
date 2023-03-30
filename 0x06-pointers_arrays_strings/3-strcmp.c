6a7fa9efec15a37cb0b46c1488edfdd468b8633a
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-03-30 15:36:25 -0700

    String Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@
 int strcmp(char *s1, char *s2)
 {
 int a;
-for (a = 0; s1[a] != '\0' && s2[a] != '\0' && s1[a] + s2[a]; a++)
+for (a = 0; s1[a] != '\0' && s2[a] != '\0' && s1[a] == s2[a]; a++)
 ;
 return (s1[a] - s2[a]);
 }

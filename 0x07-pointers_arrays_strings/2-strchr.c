1b5815250df668c1a4f380dbee0a520f9f17fbd7
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-04-04 17:40:29 -0700

    String Character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locate a string character
+ * @s: the string
+ * @c: the character
+ * Return: c
+ */
+
+char *_strchr(char *s, char c)
+{
+for (; *s != '\0'; s++)
+{
+if (*s == c)
+return (s);
+}
+return (NULL);
+}

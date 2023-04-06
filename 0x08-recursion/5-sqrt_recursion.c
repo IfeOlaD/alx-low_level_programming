8747222df8227410d5c7799bc2e1152fc8faff16
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-04-06 12:49:10 -0700

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,26 @@
 #include "main.h"
+/**
+ * _sqrt_recursion - prints the square root of given number
+ * @n: the first int
+ *
+ * Return: 0
+ */
+
+int _sqrt_recursion(int n, int a)
+{
+if (a * a == n)
+{
+return (a);
+}
+else if (a * a > n)
+{
+return (-1);
+}
+else
+{
+return (_sqrt_recursion(n, a + a));
+}
+}
 
 /**
  * _sqrt_recursion - prints the square root of a given number

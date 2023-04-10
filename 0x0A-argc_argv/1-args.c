7718676151059c4b39275a560884b44955c18aa2
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-04-10 13:35:44 -0700

    Arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,6 +9,14 @@
 
 int main(int argc __attribute__((unused)), char **argv)
 {
+int a;
+if (argc == 1)
 printf("%d\n", argc - 1);
+else
+{
+for (a = 0; *argv; a++, argv++)
+;
+printf("%d\n", a - 1);
+}
 return (0);
 }

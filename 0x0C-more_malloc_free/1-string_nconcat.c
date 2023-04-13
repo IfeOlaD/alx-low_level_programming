3151b4d8cc684ba8d5ec9fc6c87eba137945a3c6
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-04-13 16:10:39 -0700

    Contatenate String

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - contatenate 2 strings
+ * @n: the unsigned int
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *result;
+unsigned int len1 = 0, len2 = 0;
+
+if (s1 != NULL)
+len1 = strlen(s1);
+if (s2 != NULL)
+len2 = strlen(s2);
+
+if (n >= len2)
+n = len2;
+
+result = malloc(len1 + n + 1);
+if (result == NULL)
+return (NULL);
+
+if (s1 != NULL)
+strcpy(result, s1);
+if (s2 != NULL)
+strncpy(result + len1, s2, n);
+
+result[len + n] = '\0';
+return (result);
+}

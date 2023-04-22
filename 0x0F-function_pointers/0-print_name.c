5a69cb5adbe4cdf27a37318ad58e394161fac391
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-04-22 12:14:00 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,3 +11,30 @@ void print_name(char *name, void(*f)(char *))
 {
 f(name);
 }
+
+/**
+ * print_my_name - the function
+ * @name: the name to be printed
+ * Return: 0
+ */
+
+void print_my_name(char *name)
+{
+int a;
+for (a = 0; name[a]; a++)
+{
+printf("%c", name[a]);
+}
+printf("\n");
+}
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+print_name("Legend", print_my_name);
+return (0);
+}

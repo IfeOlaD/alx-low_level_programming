3723dd4048c2d183cb4be389e0318cab7a10e0dd
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-05-02 10:13:40 -0700

    Free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and set the head to NULL
+ * @head: pointer to the head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t **temp;
+
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+}
+*head = NULL;
+}

197a4e7263e4ecc126e9fc88c19ddc7bc63f554b
Olorunnimbe Amigun <amigunoio@gmail.com>
2023-05-25 14:00:11 -0700

    Read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,86 +1,57 @@
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * read_textfile - a function that reads a text file
- * @filename: the name of the file
- * @letters: the number of letters it should read
- * Return: letter
+ * read_textfile - reads a textfile and print it to the POSIX output
+ * @filename: name of the file to readd
+ * @letters: name of the letters it should read and print
+ * Return: actual numbers of letters it could read or print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
+ssize_t n_read, n_written;
 char *buffer;
-ssize_t bytes_read;
-ssize_t bytes_written;
+
 if (filename == NULL)
-{
 return (0);
-}
+
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
 return (0);
-}
-buffer = (char *)malloc(sizeof(char) * letters);
+
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
-{
-close(fd);
 return (0);
-}
 
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
-{
-free(buffer);
-close(fd);
+n_written = write(STDOUT_FILENO, buffer, n_read);
+if (n_written == -1 || n_written != n_read)
 return (0);
-}
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-free(buffer);
-close(fd);
-return (0);
-}
 
-free(buffer);
 close(fd);
-
-return (bytes_written);
+free(buffer);
+return (n_written);
 }
 
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: argument count
- * @argv: argument vector
+ * main - entry point
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
-const char *filename;
-size_t letters;
-ssize_t bytes_written;
+ssize_t n_letters;
 
-if (argc != 2)
-{
-fprintf(stderr, "%s <filename>\n", argv[0]);
-return (1);
-}
-filename = argv[1];
-letters = 1024;
+n_letters = read_textfile("myfile.txt", 1024);
 
-bytes_written = read_textfile(filename, letters);
-if (bytes_written == 0)
-{
-fprintf(stderr, "%s\n", filename);
+if (n_letters == 0)
 return (1);
-}
+
 return (0);
 }

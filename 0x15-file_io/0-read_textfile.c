645fbca8740f5601f38a1503c440f280bf7e02c0
Ifeoluwa <amigunoio@gmail.com>
2023-10-02 14:03:36 -0700

    Read Textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -55,7 +55,7 @@ return (n_written);
  * Return: 0
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+int main(const char *filename, size_t letters)
 {
 open("test", O_RDONLY | O_CREAT);
 }
